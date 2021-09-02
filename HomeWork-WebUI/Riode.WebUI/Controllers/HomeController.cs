4cd395384e25a2c4cf15a9f7d53e8aaea1fc5c02
Eltun-Ismayilov <eltunhi@code.edu.az>
2021-09-02 04:52:02 +0400

    First Real Project

---
+++ HomeWork-WebUI/Riode.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Riode.WebUI.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contect()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contect(Contect contact)
+        {
+            return View();
+        }
+    }
+}
