2e94d90a6c0500e42a02e067e159a2e733e4edeb
Eltun-Ismayilov <eltunhi@code.edu.az>
2021-09-13 13:19:27 +0400

    Commit aplaid

--- HomeWork-WebUI/Riode.WebUI/Controllers/HomeController.cs
+++ HomeWork-WebUI/Riode.WebUI/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Riode.WebUI.Appcode;
 using Riode.WebUI.Model.DataContexts;
 using Riode.WebUI.Model.Entity;
 using Riode.WebUI.Model.Entity.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Riode.WebUI.Controllers
@@ -13,9 +18,11 @@ namespace Riode.WebUI.Controllers
     public class HomeController : Controller
     {
         readonly RiodeDbContext db;
-        public HomeController(RiodeDbContext db)
+        readonly IConfiguration configuration;
+        public HomeController(RiodeDbContext db, IConfiguration configuration)
         {
             this.db = db;
+            this.configuration = configuration;
 
         }
 
@@ -91,7 +98,8 @@ namespace Riode.WebUI.Controllers
                         massege = "Bu E-Mail evvelceden qeydiyyati edilib"
 
                     });
-                }else if(current != null && (current.EmailConfirmed ?? false==false))
+                }
+                else if (current != null && (current.EmailConfirmed ?? false == false))
                 {
                     return Json(new
                     {
@@ -101,8 +109,26 @@ namespace Riode.WebUI.Controllers
 
                     });
                 }
+
+
                 db.Subscrices.Add(model);
                 db.SaveChanges();
+
+                string token = $"subscribetoken-{model.Id}-{DateTime.Now:yyyyMMddHHmmss}"; // token yeni id goturuk
+                string path = $"{Request.Scheme}://{Request.Host}/subscribe-confirm?token={token}"; // path duzeldirik
+
+                var mailSended = configuration.SendEmail(model.Email, "Riode seyfesi gagas", $"Zehmet olmasa <a href={path}>Link</a> vasitesile abuneliyi tamamlayin");
+                if (mailSended == null)
+                {
+                    db.Database.RollbackTransaction();
+
+                    return Json(new
+                    {
+                        error = false,
+                        massege = "Email-gonderilmasi zamini xeta bas verdi!"
+
+                    });
+                }
 
                 return Json(new
                 {
@@ -119,6 +145,41 @@ namespace Riode.WebUI.Controllers
                 massege = "Sorgunuzun Icrasi zamani xeta yarandi,Zehmet olmasa yeniden yoxlayin"
 
             });
+        }
+
+        [HttpGet]
+        [Route("subscribe-confirm")]
+        public IActionResult SubscibeConfirm(string token)
+        {
+
+            Match match = Regex.Match(token, @"subscribetoken-(?<id>\d)-(?<executeTimeStamp>\d{14})");
+
+            if (match.Success)
+            {
+                int id = Convert.ToInt32(match.Groups["id"].Value);
+                string executeTimeStamp = match.Groups["executeTimeStamp"].Value;
+
+                var subsc = db.Subscrices.FirstOrDefault(s => s.Id == id);
+
+                if (subsc == null)
+                {
+                    ViewBag.ms = Tuple.Create(true, "Token xetasi");
+                    goto end;
+                }
+                if ((subsc.EmailConfirmed ?? false) == true)
+                {
+                    ViewBag.ms = Tuple.Create(true, "Artiq tesdiq edildi");
+                    goto end;
+                }
+                subsc.EmailConfirmed = true;
+                subsc.EmailConfirmedDate = DateTime.Now;
+                db.SaveChanges();
+
+                ViewBag.ms = Tuple.Create(false, "Abuneliyiniz tesdiq edildi");
+
+            }
+            end:
+            return View();
         }
 
     }
